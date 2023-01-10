05b666d7ce44a29ee06b23b10d2fe0200cc70890
helbture <122353184+helbture@users.noreply.github.com>
2023-01-10 10:14:44 +0100

    Add files via upload

---
+++ CurrencyConverterv1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CurrencyConverterv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.label1.Location = new System.Drawing.Point(88, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CURRENCY CONVERTER";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(21, 68);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(78, 54);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.btn_start.FlatAppearance.BorderSize = 0;
+            this.btn_start.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start.Font = new System.Drawing.Font("Tw Cen MT Condensed", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_start.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.btn_start.Location = new System.Drawing.Point(143, 261);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(228, 52);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start Converting";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.startConverting_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(524, 461);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Button btn_start;
+    }
+}
---
+++ CurrencyConverterv1/Form1.cs
@@ -0,0 +1,25 @@
+namespace CurrencyConverterv1
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startConverting_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            New_Form new_Form = new New_Form();
+            new_Form.ShowDialog();
+        }
+    }
+}
